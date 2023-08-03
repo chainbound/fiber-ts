77a6d05a9c60e9e9cd6293b48aa2d50660f13ccb
nicolas <48695862+merklefruit@users.noreply.github.com>
2023-08-03 09:30:14 +0200

    chore: update build file + example

--- dist/index.js
+++ dist/index.js
@@ -1,212 +1,35 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.hexToBytes = exports.bytesToHex = exports.Client = exports.FilterBuilder = exports.from = exports.to = exports.and = exports.or = void 0;
+exports.Client = void 0;
 const events_1 = require("events");
-const ethers_1 = require("ethers");
+const google_protobuf_empty_pb = __importStar(require("google-protobuf/google/protobuf/empty_pb"));
 const grpc_js_1 = require("@grpc/grpc-js");
-const util_1 = require("@ethereumjs/util");
-const tx_1 = require("@ethereumjs/tx");
-const eth_pb_1 = __importDefault(require("../protobuf/eth_pb"));
 const api_grpc_pb_1 = require("../protobuf/api_grpc_pb");
 const api_pb_1 = require("../protobuf/api_pb");
-var Operator;
-(function (Operator) {
-    Operator[Operator["AND"] = 1] = "AND";
-    Operator[Operator["OR"] = 2] = "OR";
-})(Operator || (Operator = {}));
-function or(...ops) {
-    return function (f, n) {
-        let newNode = {
-            Operator: Operator.OR,
-        };
-        if (!n) {
-            f.Root = newNode;
-        }
-        else {
-            if (n.Nodes) {
-                n.Nodes.push(newNode);
-            }
-            else {
-                n.Nodes = new Array(newNode);
-            }
-        }
-        for (let op of ops) {
-            op(f, newNode);
-        }
-    };
-}
-exports.or = or;
-function and(...ops) {
-    return function (f, n) {
-        let newNode = {
-            Operator: Operator.AND,
-        };
-        if (!n) {
-            f.Root = newNode;
-        }
-        else {
-            if (n.Nodes) {
-                n.Nodes.push(newNode);
-            }
-            else {
-                n.Nodes = new Array(newNode);
-            }
-        }
-        for (let op of ops) {
-            op(f, newNode);
-        }
-    };
-}
-exports.and = and;
-function to(to) {
-    return function (f, n) {
-        let newNode = {
-            Operand: {
-                Key: "to",
-                Value: hexToBase64(to),
-            },
-        };
-        if (!n) {
-            f.Root = newNode;
-        }
-        else {
-            if (n.Nodes) {
-                n.Nodes.push(newNode);
-            }
-            else {
-                n.Nodes = new Array(newNode);
-            }
-        }
-    };
-}
-exports.to = to;
-function from(from) {
-    return function (f, n) {
-        let newNode = {
-            Operand: {
-                Key: "from",
-                Value: hexToBase64(from),
-            },
-        };
-        if (!n) {
-            f.Root = newNode;
-        }
-        else {
-            if (n.Nodes) {
-                n.Nodes.push(newNode);
-            }
-            else {
-                n.Nodes = new Array(newNode);
-            }
-        }
-    };
-}
-exports.from = from;
-class FilterBuilder {
-    constructor(rootOp) {
-        let f = {};
-        rootOp(f, undefined);
-        this._filter = f;
-    }
-    // public get or(): FilterBuilder {
-    //   let newNode = {
-    //     Operator: Operator.OR,
-    //   };
-    //   if (!this._filter.Root) {
-    //     this._filter.Root = newNode;
-    //   } else {
-    //     if (this._next!.Nodes) {
-    //       this._next!.Nodes.push(newNode);
-    //     } else {
-    //       this._next!.Nodes = new Array();
-    //       this._next!.Nodes.push(newNode);
-    //     }
-    //   }
-    //   this._last = this._next;
-    //   this._next = newNode;
-    //   return this;
-    // }
-    // public get and(): FilterBuilder {
-    //   let newNode = {
-    //     Operator: Operator.AND,
-    //   };
-    //   if (!this._filter.Root) {
-    //     this._filter.Root = newNode;
-    //   } else {
-    //     if (this._next!.Nodes) {
-    //       this._next!.Nodes.push(newNode);
-    //     } else {
-    //       this._next!.Nodes = new Array();
-    //       this._next!.Nodes.push(newNode);
-    //     }
-    //   }
-    //   this._last = this._next;
-    //   this._next = newNode;
-    //   return this;
-    // }
-    // public get exit(): FilterBuilder {
-    //   this._next = this._last;
-    //   return this;
-    // }
-    // public from(address: string): FilterBuilder {
-    //   address = hexToBase64(address);
-    //   let newNode = {
-    //     Operand: {
-    //       Key: 'from',
-    //       Value: address,
-    //     },
-    //   };
-    //   // If there's no root yet, set it here
-    //   if (!this._filter.Root) {
-    //     this._filter.Root = newNode;
-    //     this._next = newNode;
-    //   } else {
-    //     // Else append as a child
-    //     // If we get here, we can be sure that this._next is defined
-    //     if (this._next!.Nodes) {
-    //       this._next!.Nodes.push(newNode);
-    //     } else {
-    //       this._next!.Nodes = new Array();
-    //       this._next!.Nodes.push(newNode);
-    //     }
-    //   }
-    //   return this;
-    // }
-    // public to(address: string): FilterBuilder {
-    //   address = hexToBase64(address);
-    //   let newNode = {
-    //     Operand: {
-    //       Key: 'to',
-    //       Value: address,
-    //     },
-    //   };
-    //   // If there's no root yet, set it here
-    //   if (!this._filter.Root) {
-    //     this._filter.Root = newNode;
-    //     this._next = newNode;
-    //   } else {
-    //     // Else append as a child
-    //     // If we get here, we can be sure that this._next is defined
-    //     if (this._next!.Nodes) {
-    //       this._next!.Nodes.push(newNode);
-    //     } else {
-    //       this._next!.Nodes = new Array();
-    //       this._next!.Nodes.push(newNode);
-    //     }
-    //   }
-    //   return this;
-    // }
-    toString() {
-        return JSON.stringify(this._filter, null, 2);
-    }
-    build() {
-        return new Uint8Array(Buffer.from(JSON.stringify(this._filter)));
-    }
-}
-exports.FilterBuilder = FilterBuilder;
+const types_1 = require("./types");
 class Client {
     constructor(target, apiKey) {
         this._client = new api_grpc_pb_1.APIClient(target, grpc_js_1.credentials.createInsecure());
@@ -242,11 +65,25 @@ class Client {
         return new TxStream(this._client, this._md, protoFilter);
     }
     /**
-     * subscribes to the new blocks stream.
-     * @returns {BlockStream} - emits new blocks as events
+     * subscribes to the new execution headers stream.
+     * @returns {ExecutionHeaderStream} - emits new blocks as events (without transactions)
      */
-    subscribeNewBlocks() {
-        return new BlockStream(this._client, this._md);
+    subscribeNewExecutionHeaders() {
+        return new ExecutionHeaderStream(this._client, this._md);
+    }
+    /**
+     * subscribes to the new execution payloads stream.
+     * @returns {ExecutionPayloadStream} - emits new blocks as events (with transactions)
+     */
+    subscribeNewExecutionPayloads() {
+        return new ExecutionPayloadStream(this._client, this._md);
+    }
+    /**
+     * subscribes to the new beacon blocks stream.
+     * @returns {BeaconBlockStream} - emits new beacon blocks as events
+     */
+    subscribeNewBeaconBlocks() {
+        return new BeaconBlockStream(this._client, this._md);
     }
     /**
      * sends a transaction
@@ -255,7 +92,7 @@ class Client {
      */
     async sendTransaction(tx) {
         return new Promise((resolve, reject) => {
-            this._txStream.write(toProto(tx), this._md, (err) => {
+            this._txStream.write((0, types_1.toProtoTx)(tx), this._md, (err) => {
                 if (err) {
                     reject(err);
                 }
@@ -304,7 +141,7 @@ class Client {
      */
     async sendTransactionSequence(txs) {
         const sequenceMsg = new api_pb_1.TxSequenceMsg();
-        sequenceMsg.setSequenceList(txs.map(toProto));
+        sequenceMsg.setSequenceList(txs.map(types_1.toProtoTx));
         return new Promise((resolve, reject) => {
             this._txSequenceStream.write(sequenceMsg, this._md, (err) => {
                 if (err) {
@@ -381,14 +218,14 @@ class TxStream extends events_1.EventEmitter {
         const _txStream = _client.subscribeNewTxs(_filter, _md);
         _txStream.on("close", () => this.emit("close"));
         _txStream.on("end", () => this.emit("end"));
-        _txStream.on("data", (data) => this.emit("tx", fromProto(data)));
+        _txStream.on("data", (data) => this.emit("data", (0, types_1.fromProtoTx)(data)));
         _txStream.on("error", async (err) => {
             console.error(err);
             this.retry(_client, _md, _filter);
         });
     }
 }
-class BlockStream extends events_1.EventEmitter {
+class ExecutionHeaderStream extends events_1.EventEmitter {
     constructor(_client, _md) {
         super();
         this.retry(_client, _md);
@@ -406,132 +243,83 @@ class BlockStream extends events_1.EventEmitter {
                 }
             });
         });
-        const _blockStream = _client.subscribeNewBlocks(new api_pb_1.BlockFilter(), _md);
+        const _blockStream = _client.subscribeExecutionHeaders(new google_protobuf_empty_pb.Empty(), _md);
         _blockStream.on("close", () => this.emit("close"));
         _blockStream.on("end", () => this.emit("end"));
-        _blockStream.on("data", (data) => this.emit("block", this.handleBlock(data)));
-        _blockStream.on("error", async () => {
+        _blockStream.on("data", (data) => this.emit("data", this.handleExecutionPayloadHeader(data)));
+        _blockStream.on("error", async (err) => {
+            console.error("transmission error", err);
             this.retry(_client, _md);
         });
     }
-    handleBlock(block) {
-        const txList = [];
-        for (let tx of block.getTransactionsList()) {
-            txList.push(fromProto(tx));
-        }
+    handleExecutionPayloadHeader(block) {
+        return (0, types_1.fromProtoExecutionHeader)(block);
+    }
+}
+class ExecutionPayloadStream extends events_1.EventEmitter {
+    constructor(_client, _md) {
+        super();
+        this.retry(_client, _md);
+    }
+    async retry(_client, _md) {
+        const now = new Date();
+        const deadline = new Date(now.getTime() + 60 * 1000);
+        await new Promise((resolve, reject) => {
+            _client.waitForReady(deadline, (err) => {
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    resolve();
+                }
+            });
+        });
+        const _blockStream = _client.subscribeExecutionPayloads(new google_protobuf_empty_pb.Empty(), _md);
+        _blockStream.on("close", () => this.emit("close"));
+        _blockStream.on("end", () => this.emit("end"));
+        _blockStream.on("data", (data) => this.emit("data", this.handleExecutionPayload(data)));
+        _blockStream.on("error", async (err) => {
+            console.error("transmission error", err);
+            this.retry(_client, _md);
+        });
+    }
+    handleExecutionPayload(block) {
+        const header = (0, types_1.fromProtoExecutionHeader)(block.getHeader());
+        const transactions = block.getTransactionsList().map(types_1.fromProtoTx);
         return {
-            hash: bytesToHex(block.getHash()),
-            parentHash: bytesToHex(block.getParentHash()),
-            number: block.getNumber(),
-            timestamp: block.getTimestamp(),
-            prevRandao: bytesToHex(block.getPrevRandao()),
-            stateRoot: bytesToHex(block.getStateRoot()),
-            receiptRoot: bytesToHex(block.getReceiptRoot()),
-            feeRecipient: bytesToHex(block.getFeeRecipient()),
-            extraData: bytesToHex(block.getExtraData()),
-            gasLimit: ethers_1.ethers.BigNumber.from(block.getGasLimit()),
-            gasUsed: ethers_1.ethers.BigNumber.from(block.getGasUsed()),
-            logsBloom: bytesToHex(block.getLogsBloom()),
-            baseFeePerGas: ethers_1.ethers.BigNumber.from(block.getBaseFeePerGas()),
-            transactions: txList,
+            header,
+            transactions,
         };
     }
 }
-function fromProto(tx) {
-    let value = 0n;
-    if (tx.getValue()) {
-        value = BigInt("0x" + Buffer.from(tx.getValue()).toString("hex"));
+class BeaconBlockStream extends events_1.EventEmitter {
+    constructor(_client, _md) {
+        super();
+        this.retry(_client, _md);
     }
-    let to;
-    if (tx.getTo().length !== 0) {
-        to = new util_1.Address(Buffer.from(tx.getTo()));
+    async retry(_client, _md) {
+        const now = new Date();
+        const deadline = new Date(now.getTime() + 60 * 1000);
+        await new Promise((resolve, reject) => {
+            _client.waitForReady(deadline, (err) => {
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    resolve();
+                }
+            });
+        });
+        const _blockStream = _client.subscribeBeaconBlocks(new google_protobuf_empty_pb.Empty(), _md);
+        _blockStream.on("close", () => this.emit("close"));
+        _blockStream.on("end", () => this.emit("end"));
+        _blockStream.on("data", (data) => this.emit("data", this.handleBeaconBlock(data)));
+        _blockStream.on("error", async (err) => {
+            console.error("transmission error", err);
+            this.retry(_client, _md);
+        });
     }
-    else {
-        to = util_1.Address.zero();
+    handleBeaconBlock(block) {
+        return (0, types_1.fromProtoBeaconBlock)(block);
     }
-    return tx_1.TransactionFactory.fromTxData({
-        // hash: tx.getHash(),
-        // from: bytesToHex(tx.getFrom()),
-        to: to,
-        type: tx.getType(),
-        nonce: BigInt(tx.getNonce()),
-        gasLimit: BigInt(tx.getGas()),
-        data: Buffer.from(tx.getInput()),
-        chainId: BigInt(tx.getChainid()),
-        value: value,
-        gasPrice: BigInt(tx.getGasPrice()),
-        v: BigInt(tx.getV()),
-        r: BigInt("0x" + Buffer.from(tx.getR()).toString("hex")),
-        s: BigInt("0x" + Buffer.from(tx.getS()).toString("hex")),
-    });
-}
-// WARNING: if transaction is legacy, it will only work on Ethereum mainnet.
-function toProto(tx) {
-    const proto = new eth_pb_1.default.Transaction();
-    if (tx.type == 0) {
-        const legacy = tx;
-        proto.setHash(legacy.hash());
-        proto.setFrom(legacy.getSenderAddress().buf);
-        if (legacy.to) {
-            proto.setTo(legacy.to.buf);
-        }
-        proto.setType(tx.type);
-        proto.setNonce(Number(tx.nonce));
-        if (tx.data) {
-            proto.setInput(tx.data);
-        }
-        proto.setChainid(1);
-        proto.setValue(hexToBytes("0x" + tx.value.toString(16)));
-        proto.setGas(Number(tx.gasLimit));
-        proto.setGasPrice(Number(legacy.gasPrice));
-        proto.setV(Number(tx.v));
-        proto.setR(hexToBytes("0x" + tx.r.toString(16)));
-        proto.setS(hexToBytes("0x" + tx.s.toString(16)));
-    }
-    else {
-        const newtx = tx;
-        proto.setHash(newtx.hash());
-        proto.setFrom(newtx.getSenderAddress().buf);
-        if (newtx.to) {
-            proto.setTo(newtx.to.buf);
-        }
-        proto.setType(tx.type);
-        proto.setNonce(Number(tx.nonce));
-        if (tx.data) {
-            proto.setInput(newtx.data);
-        }
-        proto.setChainid(Number(newtx.chainId));
-        proto.setValue(hexToBytes("0x" + newtx.value.toString(16)));
-        proto.setGas(Number(newtx.gasLimit));
-        proto.setMaxFee(Number(newtx.maxFeePerGas));
-        proto.setPriorityFee(Number(newtx.maxPriorityFeePerGas));
-        proto.setV(Number(tx.v));
-        proto.setR(hexToBytes("0x" + tx.r.toString(16)));
-        proto.setS(hexToBytes("0x" + tx.s.toString(16)));
-    }
-    return proto;
-}
-function bytesToHex(b) {
-    return "0x" + Buffer.from(b).toString("hex");
-}
-exports.bytesToHex = bytesToHex;
-function hexToBytes(str) {
-    if (!str) {
-        return new Uint8Array();
-    }
-    if (str.substring(0, 2) !== "0x") {
-        str = "0x" + str;
-    }
-    const a = [];
-    for (let i = 2, len = str.length; i < len; i += 2) {
-        a.push(parseInt(str.substr(i, 2), 16));
-    }
-    return new Uint8Array(a);
-}
-exports.hexToBytes = hexToBytes;
-function hexToBase64(str) {
-    if (str.substring(0, 2) == "0x") {
-        str = str.slice(2);
-    }
-    return Buffer.from(str, "hex").toString("base64");
 }
