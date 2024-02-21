9d56f18100ff486751efd6bc5b9a11e639f43a48
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2024-02-21 11:26:39 +0100

    chore: restored some package.json and tsconfig.json params

---
+++ dist/types.d.ts
@@ -0,0 +1,16 @@
+import { TypedTransaction } from "@ethereumjs/tx";
+import { Address } from "@ethereumjs/util";
+export interface TransactionWithSender {
+    sender: Address;
+    transaction: TypedTransaction;
+}
+export interface TransactionResponse {
+    hash: string;
+    timestamp: number;
+}
+/**
+ *
+ * @param raw a raw transaction in RLP format
+ * @returns an Ethereumjs TypedTransaction object
+ */
+export declare function fromRLPTransaction(raw: string | Uint8Array): TypedTransaction;
