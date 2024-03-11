6bf43655af315fe4486bb7d00ff51d1052f56af5
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2024-03-11 12:24:56 +0000

    feat: support for type 3 txs

--- dist/utils.js
+++ dist/utils.js
@@ -1,9 +1,27 @@
 import { TransactionFactory } from "@ethereumjs/tx";
+import { Chain, Common, Hardfork } from "@ethereumjs/common";
+import { initKZG } from "@ethereumjs/util";
+import { createKZG } from "kzg-wasm";
+/**
+ * KZG setup, along with [`Common`] transaction options are needed
+ * to support EIP-4844 type-3 transactions.
+ *
+ * References:
+ *   - https://github.com/ethereumjs/ethereumjs-monorepo/tree/master/packages/tx#kzg-setup
+ *   - https://github.com/ethereumjs/ethereumjs-monorepo/blob/7c500b70b5f593a8811fc214a3b86085188da755/packages/tx/examples/blobTx.ts
+ */
+const kzg = await createKZG();
+initKZG(kzg, "");
+const common = new Common({
+    chain: Chain.Mainnet,
+    hardfork: Hardfork.Cancun,
+    customCrypto: { kzg },
+});
 /**
  *
  * @param raw a raw transaction in RLP format
  * @returns an Ethereumjs TypedTransaction object
  */
 export function fromRLPTransaction(raw) {
-    return TransactionFactory.fromSerializedData(raw);
+    return TransactionFactory.fromSerializedData(raw, { common });
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,24 @@
 import { TransactionFactory, TypedTransaction } from "@ethereumjs/tx";
+import { Chain, Common, Hardfork } from "@ethereumjs/common";
+import { initKZG } from "@ethereumjs/util";
+import { createKZG } from "kzg-wasm";
+
+/**
+ * KZG setup, along with [`Common`] transaction options are needed
+ * to support EIP-4844 type-3 transactions.
+ *
+ * References:
+ *   - https://github.com/ethereumjs/ethereumjs-monorepo/tree/master/packages/tx#kzg-setup
+ *   - https://github.com/ethereumjs/ethereumjs-monorepo/blob/7c500b70b5f593a8811fc214a3b86085188da755/packages/tx/examples/blobTx.ts
+ */
+const kzg = await createKZG();
+initKZG(kzg, "");
+
+const common = new Common({
+  chain: Chain.Mainnet,
+  hardfork: Hardfork.Cancun,
+  customCrypto: { kzg },
+});
 
 /**
  *
@@ -6,5 +26,5 @@ import { TransactionFactory, TypedTransaction } from "@ethereumjs/tx";
  * @returns an Ethereumjs TypedTransaction object
  */
 export function fromRLPTransaction(raw: string | Uint8Array): TypedTransaction {
-  return TransactionFactory.fromSerializedData(raw as Uint8Array);
+  return TransactionFactory.fromSerializedData(raw as Uint8Array, { common });
 }
